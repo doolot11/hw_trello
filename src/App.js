03a2c1327488d25babfb265a49599a9f19938562
doolo11 <doolot928@gmail.com>
2022-06-20 21:33:00 +0600

    trello

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import RegistrationToTrello from './components/RegistrationToTrello';
+import YourProfile from './components/YourProfile';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Route path='/' exact>
+          <RegistrationToTrello />
+        </Route>
+        <Route path='/reg' exact>
+          <YourProfile />
+        </Route>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
